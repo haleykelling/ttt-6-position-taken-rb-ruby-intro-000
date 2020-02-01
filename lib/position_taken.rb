2891862f1ad4d006c2256d2f4a85dc985072afe0
Haley Kelling <haley@kellings.com>
2020-02-01 18:10:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,9 @@
-board = ["  ", "  ", "  "]
-
 def position_taken?(board, index)
-
+  if array[index_number] == "  "
+    return true
+  elsif array[index_number] == " "
+    return true
+  else array [index_number]
+    return false
+  end
 end
